372e43659560f8a9902e7a500f6364d651d07a95
totemosouomou <totemosouomou@users.noreply.github.com>
2024-05-18 20:32:53 +0900

    ページネーションf修正

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -249,7 +249,7 @@
                 <!-- ページネーション -->
                 @if ($items->hasPages())
                     <div class="card-footer clearfix pb-0">
-                        {{ $items->appends(['search' => implode(' ', session('requestSearch'))])->links() }}
+                        {{ $items->appends(['search' => implode(' ', session('requestSearch', []))])->links() }}
                     </div>
                 @endif
             </div>
